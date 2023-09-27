5bfa64c0edd815a3b7380b8125a85401b4d3bd96
GaizkaCarrasco <145749038+GaizkaCarrasco@users.noreply.github.com>
2023-09-27 08:47:57 +0200

    Cambiado mensaje

--- ProyectoGit/src/HolaMundo.java
+++ ProyectoGit/src/HolaMundo.java
@@ -1,7 +1,7 @@
 
 public class HolaMundo {
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo, editado desde la web");
+		System.out.println("sdhjd.h.lhj.ljlkdfjhl.dfjkhbnfgj");
 	}
 	
 }
